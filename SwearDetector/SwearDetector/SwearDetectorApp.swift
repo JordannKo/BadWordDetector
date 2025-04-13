5937877be4a2c3ac9b29ab6e9bb373b71d219f6f
JordannKo <jordannn0023@gmail.com>
2025-04-13 19:28:09 +0900

    create project

---
+++ SwearDetector/SwearDetector/SwearDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwearDetectorApp.swift
+//  SwearDetector
+//
+//  Created by JordannKo on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SwearDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
